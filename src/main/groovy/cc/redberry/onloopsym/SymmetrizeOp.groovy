c89f9ad1750b5676985e08fe05c1b3ea0821a250
Stanislav Poslavsky <stvlpos@mail.ru>
2018-03-06 22:29:48 +0300

    Get changes from Vika

--- src/main/groovy/cc/redberry/onloopsym/SymmetrizeOp.groovy
+++ src/main/groovy/cc/redberry/onloopsym/SymmetrizeOp.groovy
@@ -1,3 +1,6 @@
+//Чтобы указать, что класс принадлежит определенному пакету, надо использовать директиву package,
+// после которой указывается имя пакета. У нас пакет - cc.redberry.onloopsym. Мы можем импортировать пакеты и классы
+// в проект с помощью директивы import, которая указывается после директивы package.
 package cc.redberry.onloopsym
 
 import cc.redberry.core.indexgenerator.IndexGeneratorImpl
@@ -8,61 +11,186 @@ import cc.redberry.core.tensor.Tensor
 import cc.redberry.core.utils.TensorUtils
 import cc.redberry.groovy.Redberry
 
+//Особая форма импорта - статический импорт. Для этого вместе с директивой import используется модификатор static.
+// Если классы имеют статические методы, то благодаря операции статического импорта мы можем использовать
+// эти методы без названия класса. Например, писать не Math.sqrt(20), а sqrt(20), так как функция sqrt(),
+// которая возвращает квадратный корень числа, является статической.
 import static cc.redberry.core.tensor.Tensors.simpleTensor
 import static cc.redberry.groovy.RedberryStatic.ExpandAndEliminate
 import static cc.redberry.groovy.RedberryStatic.Symmetrize
 
+//В объектно-ориентированной программе с применением классов каждый объект является «экземпляром»
+// некоторого конкретного класса. Практически класс может пониматься как некий шаблон,
+// по которому создаются объекты — экземпляры данного класса.
+//У нас class SymmetrizeOp ("Симметризовать оператор", имя класса всегда с большой буквы, если имя составное,
+// то кажд. первая буква - большая; груви чувствителен к регистру)
 class SymmetrizeOp {
-
+//Статические члены класса могут использоваться без создания объектов класса. Статические методы
+// static def covariantCommutator() и static def symmetrizePair() (первая буква названия метода
+// всегда маленькая, если имя составное, остальные буквы большие). static - модификатор,
+// def - тип возвращаемого значения. Если переменные и константы хранят некоторые
+// значения, то методы содержат собой набор операторов, которые выполняют
+// определенные действия, а в фигурные скобки заключено тело метода - все действия, которые он выполняет.
+// Параметры метода представляют собой переменные, которые определяются в сигнатуре метода и создаются при его вызове.
     static def covariantCommutator(SimpleIndices dIndices, Tensor expression) {
-        use(Redberry) {
+        //covariantCommutator - Идентификатор-метода (определяет имя метода)
+        //Этот метод создаёт коммутатор ковариантных производных.
+        //SimpleIndices dIndices, Tensor expression - входные параметры метода covariantCommutator
+        //Входные параметры — это какие-либо данные, которые передаются из других классов (в данном случае
+        // import cc.redberry.core.indices.SimpleIndices и import cc.redberry.core.tensor.Tensor)
+        // и которые  метод должен обработать.
+        //SimpleIndices и Tensor - типы переменных (из какого они класса),
+        //dIndices и expression - названия (идентификаторы) переменных
+        use(Redberry) { //указываем, что будем использовать редберри (import cc.redberry.groovy.Redberry)
+            //Создаём переменные upper и lower, присваиваем им переменную expression (из класса Tensor),
+            // которая получает индексы (.indices) верхние или нижние (.upper или .lower) и свободные (.free)
+            //def (ключевое слово) - объект (вводит переменную или метод)
+            //Для того, чтобы обратиться к члену класса, необходимо указать его имя после имени объекта через точку.
+            //indices, upper, lower, free - члены класса Redberry
             def upper = expression.indices.upper.free
             def lower = expression.indices.lower.free
+            //Есть класс SymmetrizeOp. Есть public final класс TensorUtils (который выше был импортирован в проект).
+            //В классе SymmetrizeOp вызываем метод, который описан в TensorUtils -
+            // статический метод getAllIndicesNamesT с параметром expression.
+            //Создаём переменную forbiddenIndices (запрещённые индексы), присваиваем ей этот метод.
+
+            // ЧТО ДЕЛАЕТ МЕТОД getAllIndicesNamesT ?
 
             def forbiddenIndices = TensorUtils.getAllIndicesNamesT(expression)
-            forbiddenIndices.addAll(dIndices.toArray())
-            def ig = new IndexGeneratorImpl(forbiddenIndices.toArray())
-            def dummyIndex = IndicesFactory.create(ig.generate(dIndices.get(0).type)).si
 
+            // ЧТО ДЕЛАЮТ МЕТОДЫ addAll И toArray ?
+
+            //Переменная forbiddenIndices обращается к методу addAll, в параметрах которой
+            // переменная dIndices обращается к методу toArray
+            forbiddenIndices.addAll(dIndices.toArray())
+            //Оператор new создает экземпляр (переменную ig) указанного класса (в данном случае
+            // класса IndexGeneratorImpl) и возвращает ссылку на вновь созданный объект.
+            def ig = new IndexGeneratorImpl(forbiddenIndices.toArray())
+            //Создаём переменную dummyIndex (немой индекс) и присваиваем ей статический метод create
+            // из класса IndicesFactory. В параметрах метода create переменная ig обращается к методу
+            // generate, в параметрах которого переменная dIndices обращается к ... ???
+
+            // ЧТО ДЕЛАЮТ МЕТОДЫ create И generate ? что есть get(0).type ?
+
+            //.si создаёт SimpleIndices объект — упорядоченные индексы (каждый индекс имеет чёткую позицию)
+            def dummyIndex = IndicesFactory.create(ig.generate(dIndices.get(0).type)).si
+            //Создаём переменную result, которой присваиваем значение 0.
+            //.t превращает обычные символы в компьютерный объект
             def result = 0.t
+            //ПЕРВЫЙ ЦИКЛ:
+            //int k = 0 — инициализация счетчика, k < upper.size() — условие, при котором будет выполняться цикл,
+            // ++k — изменение счетчика, далее в фигурных скобках все действия, которые происходят в цикле.
+            //Некий_тензор.size() - возвращает размер тензора, т. е. количество слагаемых в случае суммы,
+            // количество аргументов в случае функции и т. д.
+            //++k — префиксный инкремент — увеличивает значение операнда на 1, и возвращает новое значение
+            // в отл. от k++, который увеличивает, но возвращает старое значение.
             for (int k = 0; k < upper.size(); ++k) {
+                //Создаём переменную currentIndex (текущий индекс), присваиваем ей значение объекта upper.
+                //Некий_тензор[i..j] - возвращает список элементов от i-го (включительно) по j-й (не включительно)??
+                //.si создаёт SimpleIndices объект — упорядоченные индексы
                 def currentIndex = upper[k..k].si
+                //Создаём переменную riemannIndices, которой присваиваем значение суммы текущих, немых и dIndices
+                //Как сумма SimpleIndices римановы индексы также будут SimpleIndices.
                 def riemannIndices = currentIndex + dummyIndex + dIndices
+                //+= (Оператор присваивания «Добавления», он присваивает левому операнду значения правого,
+                // C += A, эквивалентно C = C + A)
+                //% (Делит левый операнд на правый операнд и возвращает остаток)
+                //>> (Бинарный оператор сдвига вправо. Значение правых операндов перемещается вправо
+                // на количество бит, заданных левых операндом)
+                //.inverted - получать инвертированные индексы
                 result += simpleTensor("R", riemannIndices) * ((currentIndex % dummyIndex.inverted) >> expression)
             }
 
             for (int k = 0; k < lower.size(); ++k) {
                 def currentIndex = lower[k..k].si
                 def riemannIndices = dummyIndex.inverted + currentIndex + dIndices
+                //-= (Оператор присваивания «Вычитания», он вычитает из правого операнда левый операнд,
+                // C -= A, эквивалентно C = C - A)
                 result -= simpleTensor("R", riemannIndices) * ((currentIndex % dummyIndex) >> expression)
             }
 
-            return result
+            return result //Методы которые возвращают значения, также условно называют функциями.
+            // Функции также отличаются тем, что мы обязательно должны использовать оператор return,
+            // после которого ставится возвращаемое значение.
         }
     }
 
+    //Этот метод симметризует выражение по паре индексов.
     static def symmetrizePair(Tensor gTensor, SimpleTensor nabla, Tensor hTensor, int iPosition) {
+        //Tensor gTensor, SimpleTensor nabla, Tensor hTensor, int iPosition - параметры метода symmetrizePair.
+        //SimpleTensor, Tensor и int - типы переменных, gTensor, nabla, hTensor и iPosition - названия переменных.
         use(Redberry) {
             def product = gTensor * nabla * hTensor
 //        if (product.indices.free.size() != 0)
 //            throw new IllegalArgumentException("bad input: $gTensor * $nabla * $hTensor")
 
+            //Симметризуем индексы в выражении по аналогии с - Симметризировать индексы a и b в выражении:
+            //def indices = '_ab'.si
+            //indices.symmetries.setSymmetric()
+            //Создаём переменную toSymmetrize, которой присваиваем переменную nabla с инвертированными индексами.
+
+            // ЧТО ТАКОЕ [iPosition, iPosition + 1] ?
+
             def toSymmetrize = nabla.indices[iPosition, iPosition + 1].inverted
+            //Свойство .symmetries позволяет определить перестановочные симметрии индексов. Оно
+            // возвращает контейнер перестановок и соответствующую группу PermutationGroup.
+            //Метод setSymmetric() устанавливает симметрии.
             toSymmetrize.symmetries.setSymmetric()
+            //Создаём переменную higher, которой присваиваем произведение, в котором... ???
+
+            // ЧТО ПРОИСХОДИТ С gTensor ?
+
+            //Symmetrize[indices] — делает симметрии выражения одинаковыми с симметриями индексов,
+            // делает выражение симметричным только по заданным индексам, также умножит результат
+            // на симметрийный коэффициент.
             def higher = (Symmetrize[toSymmetrize] >> gTensor) * nabla * hTensor
 
-
+            //Создаём переменную lNabla, а дальше идёт условный (тернарный) оператор.
+            //== (Проверяет, равны или нет значения двух операндов, если да, то условие становится истинным)
+            //Тернарный оп-р (?:), переменная lNabla = (выражение) ? значение if true : значение if false
+            // Тернарная операция имеет следующий синтаксис: [первый операнд - условие] ? [второй операнд]
+            // : [третий операнд]. Таким образом, в этой операции участвуют сразу три операнда.
+            // В зависимости от условия тернарная операция возвращает второй или третий операнд: если
+            // условие равно true, то возвращается второй операнд; если условие равно false, то третий.
+            // iPosition == 0 — первый операнд, 1.t — второй операнд
+            // simpleTensor(nabla.stringName, nabla.indices[0..(iPosition - 1)]) — третий операнд
+            // nabla.indices[0..(iPosition - 1)] — возвращает список элементов от 0-го по (iPosition - 1) ??
             def lNabla = iPosition == 0 ? 1.t : simpleTensor(nabla.stringName, nabla.indices[0..(iPosition - 1)])
-            def rNabla = iPosition == nabla.indices.size() - 2 ? 1.t : simpleTensor(nabla.stringName, nabla.indices[(iPosition + 2)..(nabla.indices.size() - 1)])
-            def nNabla = ((lNabla.indices + rNabla.indices).size() == 0) ? 1.t : simpleTensor(nabla.stringName, lNabla.indices + rNabla.indices)
-            def subs = (lNabla * rNabla).eq(nNabla)
+            //Создаём переменную rNabla, дальше условный оператор.
+            // iPosition == nabla.indices.size() - 2 — первый операнд, справа SimpleTensor nabla
+            // получает индексы (.indices), .size() — возвращает размер тензора.
+            // 1.t — второй операнд
+            // simpleTensor(nabla.stringName, nabla.indices[(iPosition + 2)..(nabla.indices.size() - 1)]) — 3-тий оп.
+            // simpleTensor — метод из класса Tensors, первый аргумент nabla.stringName,
+            // второй агрумент — nabla.indices[(iPosition + 2)..(nabla.indices.size() - 1)],
+            // возвращает список элементов от (iPosition + 2)-го по (nabla.indices.size() - 1).
 
+            // ЧТО ТАКОЕ stringName ?
+
+            def rNabla = iPosition == nabla.indices.size() - 2 ? 1.t : simpleTensor(nabla.stringName, nabla.indices[(iPosition + 2)..(nabla.indices.size() - 1)])
+            //Создаём переменную nNabla, дальше условный оператор.
+            // ((lNabla.indices + rNabla.indices).size() == 0) — первый операнд, 1.t — второй операнд.
+            // simpleTensor(nabla.stringName, lNabla.indices + rNabla.indices) — 3-тий операнд.
+            def nNabla = ((lNabla.indices + rNabla.indices).size() == 0) ? 1.t : simpleTensor(nabla.stringName, lNabla.indices + rNabla.indices)
+            //Создаём переменную subs, присваиваем ей... ???
+
+            // ЧТО ДЕЛАЕТ МЕТОД eq ?
+
+            def subs = (lNabla * rNabla).eq(nNabla)
+            //Создаём переменную lower... ???
             def lower = covariantCommutator(nabla.indices[iPosition, iPosition + 1], rNabla * hTensor) * lNabla * gTensor / 2.t
+            // <<= (Оператор присваивания «Сдвиг влево», C << = 2, это как C = C << 2).
+            // & (Бинарный оператор AND копирует бит в результат, если он существует в обоих операндах).
+            //ExpandAndEliminate разлагает произведение сумм и положительных целых степеней и,
+            // по ходу дела, устраняет метрические тензоры и дельты Кронекера.
+            //ExpandAndEliminate эквивалентен последовательному применению Expand & EliminateMetrics.
+            //ExpandAndEliminate равен Expand [EliminateMetrics] & EliminateMetrics.
+            //Когда нет метрических тензоров или дельт Кронекера, ExpandAndEliminate работает так же, как Expand.
+            //ExpandAndEliminate [simplifications] или ExpandAndEliminate [[Simplifications: simplifications]]
+            // будет применять дополнительные упрощения на каждом уровне процедуры разложения.
             lower <<= ExpandAndEliminate & subs
 
             return higher + lower
         }
     }
-
-
 }
--- src/test/groovy/cc/redberry/onloopsym/SymmetrizeOpTest.groovy
+++ src/test/groovy/cc/redberry/onloopsym/SymmetrizeOpTest.groovy
@@ -1,5 +1,6 @@
 package cc.redberry.onloopsym
 
+import cc.redberry.core.context.CC
 import cc.redberry.core.utils.TensorUtils
 import cc.redberry.groovy.Redberry
 import org.junit.Test
@@ -9,20 +10,30 @@ import static cc.redberry.core.tensor.Tensors.setSymmetric
 import static cc.redberry.groovy.RedberryStatic.*
 import static cc.redberry.onloopsym.SymmetrizeOp.symmetrizePair
 
-/**
- *
- * @author Stanislav Poslavsky
- * @since 1.0
- */
+//Проверяем вычисления для работы "Quantum properties of affine-metric gravity with the cosmological term".
+//Создаём класс SymmetrizeOpTest
 class SymmetrizeOpTest {
 
-    @Test
-    void test1() {
-        use(Redberry) {
+    @Test // ЭТО ЧТО ???
+    void test1() { //Ключевое слово void указывает на то, что метод ничего не возвращает. Условно методы,
+        // которые не возвращают никакого значения, называются процедурами.
+        use(Redberry) {//указываем, что будем использовать редберри
+            CC.resetTensorNames(1) //фиксирует порядок слагаемых в выводимом результате
+            //Устанавливаем симметрии тензора Римана. Метод addSymmetry имеет два аргумента: простой тензор
+            // (или его строковое представление) и перестановку. Redberry имеет внутреннее представление
+            // перестановок и групп перестановок. Чтобы преобразовать массив в перестановку, можно
+            // использовать свойство .p, которое следует за массивом, записанным в однострочном
+            // или непересекающемся цикле. Минус (используемый в последней строке) преобразует
+            // симметрию в антисимметрию и наоборот.
+
+            // ПОЧЕМУ У НАС addSymmetries, А НЕ addSymmetry ? ПОТОМУ ЧТО ДОБАВЛЯЕМ ДВЕ СИММЕТРИИ?
+
             addSymmetries 'R_abcd', -[[0, 1]].p, [[0, 2], [1, 3]].p
+            //Устанавливаем симметрии тензоров R и H.
             setSymmetric 'R_ab'
             setSymmetric 'H_ab'
-
+            // Определяем переменные (были в методе symmetrizePair класса SymmetrizeOp)
+            //.t превращает обычные символы в компьютерный объект
             def gTensor = 'G^ab_ij'.t
             def nabla = 'N_ab'.t
             def hTensor = 'H^ij'.t
@@ -35,24 +46,41 @@ class SymmetrizeOpTest {
 //    println(gTensor.indices[2])
 
 //    println(covariantCommutator('_mn'.si, "T^a_b".t))
-
+            // Определяем четырёхмерную единицу
             'd_abcd := (1/2)*(d_ab*d_cd + d_ac*d_bd)'.t
-
+            // Вводим оператор, соответствующий D2. Почему "op" - серое? Куда оно ссылается?
             def op = 'd_ab^gd * N_a^a + (1/2)*(d_a^g * N_b^d + d_b^g * N_a^d + d_a^d * N_b^g + d_b^d * N_a^g)'.t
+            // Вводим матрицу К, соответствующую оператору D2.
             def k = 'K_c^e_{ab}^{gd} = ((1/2)*d_{a}^{e}*d_{b}^{g}*d^{d}_{c}+(1/2)*d^{g}_{c}*d_{a}^{d}*d_{b}^{e}+(1/2)*d_{a}^{g}*d^{d}_{c}*d_{b}^{e}+(1/2)*d_{a}^{e}*d^{g}_{c}*d_{b}^{d}+d^{e}_{c}*d_{ab}^{gd})'.t
 //    println(Collect['N_ab'] >> op)
+            // << (Бинарный оператор сдвига влево. Значение левых операндов перемещается влево на количество бит,
+            // заданных правым операндом.)
 
+            // ЧТО ПРОИСХОДИТ ?
+
+            //Создаём переменную symmetrized и ... ??
             def symmetrized = symmetrizePair('K^ab_{ijpq}'.t, 'N_ab'.t, 'H^ij'.t, 0) << k
+            // <<= (Оператор присваивания «Сдвиг влево», C << = 2, это как C = C << 2)
+            //ExpandAndEliminate разлагает произведение сумм и положительных целых степеней и,
+            // по ходу дела, устраняет метрические тензоры и дельты Кронекера.
             symmetrized <<= ExpandAndEliminate
+            // Тензор Риччи
             symmetrized <<= 'R^a_bac = R_bc'.t
+            // Скалярная кривизна
             symmetrized <<= 'R^a_a = R'.t
 
-
+            //Collect[var1, var2] собирает вместе члены, имеющие те же степени, что и указанные выражения.
+            // В случае тензорных выражений Collect введёт дельты Кронекера или метрические тензоры, чтобы
+            // «???» индексы и разложить тензорные части.
+            //В случае скалярных выражений поведение Collect аналогично другим CAS.
+            //EliminateDueSymmetries удаляет части выражения, которые равны нулю из-за симметрии.
             symmetrized <<= Collect['N_ab'.t, 'H^ij'.t] & EliminateDueSymmetries
-
+            //Статический метод info из класса TensorUtils с параметром symmetrized возвращает информацию
+            // об объекте symmetrized.
             println TensorUtils.info(symmetrized)
-
+            //Произведение матрицы W и поля H: W^{a}_{p,q,c}*H^{c}_{a} !НЕ СОВПАДАЕТ!
             println(symmetrized[0])
+            // Что он выводит тут ?
             println(symmetrized[1])
         }
     }
